0cbc93aede5baf03edd5bcc24a0a97322a5b139a
kevin <kevin@lmve.net>
2023-08-06 20:17:22 +0800

    Signed-off-by: kevin <kevin@lmve.net>

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from './router'
+import axios from 'axios'
 
 const app=createApp(App)
 app.use(router)
+app.config.globalProperties.$http=axios
 app.mount('#app')
